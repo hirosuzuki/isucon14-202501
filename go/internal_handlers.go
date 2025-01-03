f7d8e110c9b9435ce3e7c0e29c84080573a64db7
hirosuzuki <hiro.suzuki.oct10@gmail.com>
2025-01-04 03:21:29 +0900

    完了時間をベースとしたマルチマッチング実装

--- go/internal_handlers.go
+++ go/internal_handlers.go
@@ -1,52 +1,120 @@
 package main
 
 import (
-	"database/sql"
-	"errors"
+	"fmt"
+	"log/slog"
 	"net/http"
+	"sort"
 )
+
+var chairsx_sql = `
+WITH
+active_rides AS (
+  SELECT ride_id, COUNT(chair_sent_at) chair_sent_count FROM ride_statuses GROUP BY ride_id HAVING chair_sent_count < 6
+),
+active_chairs AS (
+  SELECT chair_id FROM rides WHERE chair_id IS NOT NULL AND id IN (SELECT ride_id FROM active_rides)
+)
+SELECT chairs.id, chairs_ex.latitude, chairs_ex.longitude,
+  model, speed
+FROM chairs
+  LEFT JOIN chairs_ex ON chairs_ex.id = chairs.id
+  LEFT JOIN chair_models ON chair_models.name = model
+WHERE is_active = true
+  AND chairs.id NOT IN (SELECT chair_id FROM active_chairs)
+`
+
+type Chairx struct {
+	ID        string  `db:"id"`
+	Latitude  *int    `db:"latitude"`
+	Longitude *int    `db:"longitude"`
+	Model     string  `db:"model"`
+	Speed     int     `db:"speed"`
+	Status    *string `db:"status"`
+}
+
+type MatchItem struct {
+	Proirity       int
+	RideID         string
+	RideArea       int
+	ChairID        string
+	ChairArea      int
+	WaitTime       int
+	PickupDistance int
+	DriveDistance  int
+	Speed          int
+}
 
 // このAPIをインスタンス内から一定間隔で叩かせることで、椅子とライドをマッチングさせる
 func internalGetMatching(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
-	// MEMO: 一旦最も待たせているリクエストに適当な空いている椅子マッチさせる実装とする。おそらくもっといい方法があるはず…
-	ride := &Ride{}
-	if err := db.GetContext(ctx, ride, `SELECT * FROM rides WHERE chair_id IS NULL ORDER BY id LIMIT 1`); err != nil {
-		if errors.Is(err, sql.ErrNoRows) {
-			w.WriteHeader(http.StatusNoContent)
-			return
-		}
+
+	rides := &[]Ride{}
+	if err := db.SelectContext(ctx, rides, `SELECT * FROM rides WHERE chair_id IS NULL`); err != nil {
 		writeError(w, http.StatusInternalServerError, err)
 		return
 	}
 
-	matched := &Chair{}
-	empty := false
-	for i := 0; i < 10; i++ {
-		if err := db.GetContext(ctx, matched, "SELECT * FROM chairs INNER JOIN (SELECT id FROM chairs WHERE is_active = TRUE ORDER BY RAND() LIMIT 1) AS tmp ON chairs.id = tmp.id LIMIT 1"); err != nil {
-			if errors.Is(err, sql.ErrNoRows) {
-				w.WriteHeader(http.StatusNoContent)
-				return
-			}
-			writeError(w, http.StatusInternalServerError, err)
-		}
-
-		if err := db.GetContext(ctx, &empty, "SELECT COUNT(*) = 0 FROM (SELECT COUNT(chair_sent_at) = 6 AS completed FROM ride_statuses WHERE ride_id IN (SELECT id FROM rides WHERE chair_id = ?) GROUP BY ride_id) is_completed WHERE completed = FALSE", matched.ID); err != nil {
-			writeError(w, http.StatusInternalServerError, err)
-			return
-		}
-		if empty {
-			break
-		}
-	}
-	if !empty {
+	if len(*rides) == 0 {
 		w.WriteHeader(http.StatusNoContent)
 		return
 	}
 
-	if _, err := db.ExecContext(ctx, "UPDATE rides SET chair_id = ? WHERE id = ?", matched.ID, ride.ID); err != nil {
+	chairsx := &[]Chairx{}
+	if err := db.SelectContext(ctx, chairsx, chairsx_sql); err != nil {
 		writeError(w, http.StatusInternalServerError, err)
 		return
+	}
+
+	if len(*chairsx) == 0 {
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+
+	var matchItems = make([]MatchItem, 0)
+	for _, chair := range *chairsx {
+		if chair.Latitude == nil || chair.Longitude == nil {
+			continue
+		}
+		chairArea := *chair.Latitude < 200
+		for _, ride := range *rides {
+			rideArea := ride.PickupLatitude < 200
+			if rideArea != chairArea {
+				continue
+			}
+			driveDistance := abs(ride.PickupLatitude-ride.DestinationLatitude) + abs(ride.PickupLongitude-ride.DestinationLongitude)
+			pickupDistance := abs(ride.PickupLatitude-*chair.Latitude) + abs(ride.PickupLongitude-*chair.Longitude)
+			speed := chair.Speed
+			matchItems = append(matchItems, MatchItem{
+				RideID:         ride.ID,
+				ChairID:        chair.ID,
+				PickupDistance: pickupDistance,
+				DriveDistance:  driveDistance,
+				Speed:          speed,
+				Proirity:       (pickupDistance + driveDistance) / speed,
+			})
+		}
+	}
+
+	sort.Slice(matchItems, func(i, j int) bool {
+		return matchItems[i].Proirity < matchItems[j].Proirity
+	})
+
+	matchedRideIDs := make(map[string]bool)
+	matchedChairIDs := make(map[string]bool)
+	for _, matchItem := range matchItems {
+		if matchedRideIDs[matchItem.RideID] || matchedChairIDs[matchItem.ChairID] {
+			continue
+		}
+		matchedRideIDs[matchItem.RideID] = true
+		matchedChairIDs[matchItem.ChairID] = true
+		slog.Info(fmt.Sprintf("Matching ride %s with chair %s / Prio %d", matchItem.RideID, matchItem.ChairID, matchItem.Proirity))
+		if _, err := db.ExecContext(ctx, "UPDATE rides SET chair_id = ? WHERE id = ?", matchItem.ChairID, matchItem.RideID); err != nil {
+			writeError(w, http.StatusInternalServerError, err)
+			return
+		}
+		signalCondByID(matchItem.ChairID)
+
 	}
 
 	w.WriteHeader(http.StatusNoContent)
